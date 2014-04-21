e85d02e426ef91d86ef27c7b159a55a9910bdaa5
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-04-21 10:50:33 -0400

    Reworked things to introduce Runtime.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var interceptor = require('./lib/moduleInterceptor.js');
 
 // TODO: module to load 'plugins'
-interceptor.intercept('express', require('./lib/glimpseExpress.js'));
+interceptor.intercept('express', require('./lib/express/index.js'));
